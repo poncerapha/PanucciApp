05038461e9b4e820a32d5d5eb805409333ed883b
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-11-06 06:57:43 -0300

    feat: setup project

--- app/src/main/java/com/example/panucci/MainActivity.kt
+++ app/src/main/java/com/example/panucci/MainActivity.kt
@@ -1,46 +1,140 @@
 package com.example.panucci
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PointOfSale
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.panucci.sampledata.bottomAppBarItems
+import com.example.panucci.sampledata.sampleProductWithImage
+import com.example.panucci.sampledata.sampleProducts
+import com.example.panucci.ui.components.BottomAppBarItem
+import com.example.panucci.ui.components.PanucciBottomAppBar
 import com.example.panucci.ui.theme.PanucciTheme
+import com.example.panucci.ui.screens.CheckoutScreen
+import com.example.panucci.ui.screens.DrinksListScreen
+import com.example.panucci.ui.screens.HighlightsListScreen
+import com.example.panucci.ui.screens.MenuListScreen
+import com.example.panucci.ui.screens.ProductDetailsScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val initialScreen = "Destaques"
+            val screens = remember {
+                mutableStateListOf(initialScreen)
+            }
+            Log.i("MainActivity", "onCreate: screens ${screens.toList()}")
+            val currentScreen = screens.last()
+            BackHandler(screens.size > 1) {
+                screens.removeLast()
+            }
             PanucciTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var selectedItem by remember(currentScreen) {
+                        val item = bottomAppBarItems.find { currentScreen == it.label }
+                        mutableStateOf(item)
+                    }
+                    PanucciApp(
+                        bottomAppBarItemSelected = selectedItem ?: bottomAppBarItems.first(),
+                        onBottomAppBarItemSelectedChange = {
+                            selectedItem = it
+                            screens.add(it.label)
+                        },
+                        onFabClick = {
+                            screens.add("Pedido")
+                        }) {
+                        when (currentScreen) {
+                            "Destaques" -> HighlightsListScreen(
+                                products = sampleProducts,
+                                onOrderClick = {
+                                    screens.add("Pedido")
+                                },
+                                onProductClick = {
+                                    screens.add("DetalhesProduto")
+                                }
+                            )
+                            "Menu" -> MenuListScreen(
+                                products = sampleProducts
+                            )
+                            "Bebidas" -> DrinksListScreen(
+                                products = sampleProducts + sampleProducts
+                            )
+                            "DetalhesProduto" -> ProductDetailsScreen(
+                                product = sampleProductWithImage
+                            )
+                            "Pedido" -> CheckoutScreen(products = sampleProducts)
+                        }
+                    }
                 }
             }
         }
     }
+
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PanucciApp(
+    bottomAppBarItemSelected: BottomAppBarItem = bottomAppBarItems.first(),
+    onBottomAppBarItemSelectedChange: (BottomAppBarItem) -> Unit = {},
+    onFabClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Ristorante Panucci")
+                },
+            )
+        },
+        bottomBar = {
+            PanucciBottomAppBar(
+                item = bottomAppBarItemSelected,
+                items = bottomAppBarItems,
+                onItemChange = onBottomAppBarItemSelectedChange,
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onFabClick
+            ) {
+                Icon(
+                    Icons.Filled.PointOfSale,
+                    contentDescription = null
+                )
+            }
+        }
+    ) {
+        Box(
+            modifier = Modifier.padding(it)
+        ) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun PanucciAppPreview() {
     PanucciTheme {
-        Greeting("Android")
+        Surface {
+            PanucciApp {}
+        }
     }
 }
--- app/src/main/java/com/example/panucci/ui/theme/Theme.kt
+++ app/src/main/java/com/example/panucci/ui/theme/Theme.kt
@@ -3,11 +3,7 @@ package com.example.panucci.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
@@ -41,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 fun PanucciTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -49,16 +45,16 @@ fun PanucciTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window?.let { window ->
+                window.statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            }
         }
     }
 
