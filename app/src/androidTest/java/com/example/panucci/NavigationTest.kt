19a4d6f9a7ddbce9c715fc7228a58d65a35554eb
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-11-09 18:34:27 -0300

    feat: create navigation test

---
+++ app/src/androidTest/java/com/example/panucci/NavigationTest.kt
@@ -0,0 +1,35 @@
+package com.example.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.panucci.navigation.PanucciNavHost
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        composeTestRule
+            .onNodeWithText("Destaques do dia")
+            .assertIsDisplayed()
+    }
+}
